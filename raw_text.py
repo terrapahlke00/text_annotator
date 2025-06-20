1a05ae97d6b6d7b83a8ad3bb611a8c406420ae99
terrapahlke00 <terrapahlke@rogers.com>
2025-06-20 18:16:57 +0000

    Updated raw_text.py to improve text processing

--- raw_text.py
+++ raw_text.py
@@ -1,34 +1,5 @@
 # store raw text here
 
 raw_text = """
-We are looking for an experienced and highly capable Data Scientist to help us drive step function improvements in our ads delivery at Pinterest. In this role, you will:
-
-Strategy: Evolve our strategy by working closely with product and engineering leaders to build on past learnings and shape our platform's future.
-Experimentation: Improve our experimentation capabilities and tools for assessing our ad delivery system, advise on best practices, identify flaws, and build tools and frameworks for experiment analysis.
-Leadership: Lead and mentor peers, improving team output through continuous, candid feedback that recognizes strengths and identifies areas for growth. Be a technical leader for junior data scientists based in the Toronto office.
-Communication: Clearly and concisely communicate complex analytical findings and insights to stakeholders with varying technical backgrounds.
-Continuous Learning: Stay informed on the latest developments in data science, causal inference, and advertising technology to drive innovation.
-
-
-What We’re Looking For
-
-8+ years of combined post-graduate academic and industry experience applying scientific methods to solve real-world problems on web-scale data
-Bachelor’s or Master’s degree, or PhD, in a relevant field such as Data Science, or equivalent experience
-Proven ability to apply scientific methods to solve real-world problems on web-scale data
-Expertise in SQL and Python
-Strong business and product sense: delight in shaping vague questions into well-defined analyses and success metrics that drive business decisions
-Excellent communication skills: able to lead initiatives across multiple product areas and communicate findings with leadership and product teams 
-Experience leading key technical projects and substantially influencing the scope and output of others
-Experience in digital ad delivery stacks preferred
-
-
-Relocation Statement:
-
-This position is not eligible for relocation assistance. Visit our PinFlex page to learn more about our working model.
-
-
-In-Office Requirement Statement
-
-We let the type of work you do guide the collaboration style. That means we're not always working in an office, but we continue to gather for key moments of collaboration and connection.
-This role will need to be in the office for in-person collaboration 1-2 times per half and therefore needs to be situated in a commutable distance from the Toronto office (85 Richmond St. W)
+Raw text goes here, in this python file.
 """
